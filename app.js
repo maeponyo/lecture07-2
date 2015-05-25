2c5c2fb2b1be2d2d239883475b3cb1bc7565ac7c
N.Shimizu <chikoski@gmail.com>
2015-05-25 17:05:16 +0900

    サンプルコードの初期状態を追加

---
+++ app.js
@@ -0,0 +1,37 @@
+navigator.getUserMedia = 
+  navigator.getUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.webkitGetUserMedia;
+
+var app = {
+  preview: document.querySelector("video"),
+  stream: null
+};
+
+var MEDIA_CONSTRAINT = {
+  video: true,
+  audio: true
+};
+
+function streamAquired(stream){
+  app.stream = stream;
+  app.preview.src = window.URL.createObjectURL(stream);
+  app.preview.play();
+}
+
+function streamAquisitionFailed(error){
+  console.log(error);
+}
+
+function initialize(){
+  navigator.getUserMedia(MEDIA_CONSTRAINT, streamAquired, streamAquisitionFailed);
+}
+
+function unload(){
+  if(app.stream != null){
+    app.stream.stop();
+    app.stream = null;
+  }
+}
+
+window.addEventListener("unload", unload);
